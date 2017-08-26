3f55a10e3e2474480a21ec508ce80e53c7d04c38
Assunta Oropallo <assunta.oropallo@gmail.com>
2017-08-26 18:18:16 +0200

    add image to detail activity

--- app/src/main/java/com/oropallo/assunta/demosantagata/DetailActivity.java
+++ app/src/main/java/com/oropallo/assunta/demosantagata/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.oropallo.assunta.demosantagata;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,9 +13,14 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        String title= intent.getStringExtra("Name");
+
         setContentView(R.layout.activity_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
